d4fbf439559f2aef2b0f6c2d66a19cfd9310e4ec
grigsha <144799664+grigsha@users.noreply.github.com>
2024-05-13 14:41:13 +0300

    Update Hello-world.cpp

--- Hello-world.cpp
+++ Hello-world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <string>
+#include <string> //str
 int main() {
-string name; //name;
-std::cout << "enter name: " << std::endl; //show name
-std::cin >> name; //write name
-std::cout << "Hello world " << name << std::endl; //result
+string name; //name
+std::cout << "enter name: " << std::endl; //show name.
+std::cin >> name; //напишите имя
+std::cout << "Hello world " << name << std::endl; //result.
 }
