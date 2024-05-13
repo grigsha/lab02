20ff93496cd43791915b51239424c2d33296e0db
grigsha <mari.aa05@mail.ru>
2024-05-13 13:22:57 +0300

    Changed code style in cpp file

--- Hello-world.cpp
+++ Hello-world.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
-int main() {
-string name; //name;
-std::cout << "enter name: " << std::endl; //show name
-std::cin >> name; //write name
-std::cout << "Hello world " << name << std::endl; //result
+int
+main()
+{
+  string name;                                      // name;
+  std::cout << "enter name: " << std::endl;         // show name
+  std::cin >> name;                                 // write name
+  std::cout << "Hello world " << name << std::endl; // result
 }
